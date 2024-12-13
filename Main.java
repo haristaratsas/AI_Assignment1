a4498c02b750e699fb70109ace1d5fb1af18be68
Haris Taratsas <haristaratsas@gmail.com>
2024-12-13 20:33:08 +0200

    Add initial implementation of forward chaining and related classes

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main{
 
 	public static void main(String [] args) {
+
         Scanner in = new Scanner(System.in);
         int N = Integer.parseInt(args[0]);
         int M = Integer.parseInt(args[1]);
